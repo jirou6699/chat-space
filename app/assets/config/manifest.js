9bdfe5432722efc5d357fcccb924d7fbbaa8c0e4
jirou6699 <shundom09@gmail.com>
2020-08-15 13:12:58 +0900

    chat-space-Ajax

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,3 @@
 //= link_tree ../images
+//= link_directory ../javascripts .jp
 //= link_directory ../stylesheets .css
---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="MessageBox">
+          <div class="MessageInfo">
+            <div class="MessageInfo--name">
+              ${message.user_name}
+            </div>
+            <div class="MessageInfo--date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="MessageText">
+            <p class="MessageText__content">
+              ${message.text}
+            </p>
+            <img class="MessageText__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="MessageBox">
+        <div class="MessageInfo">
+          <div class="MessageInfo--name">
+            ${message.user_name}
+          </div>
+          <div class="MessageInfo--date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="MessageText">
+          <p class="MessageText__content">
+            ${message.text}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $(".Form").on("submit", function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $(".Chat-main__message-list").append(html);
+      $('form')[0].reset();
+      $('.Chat-main__message-list').animate({ scrollTop: $('.Chat-main__message-list')[0].scrollHeight});
+      $('.SendButton').prop('disabled', false);
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
+    });
+  });
+});
