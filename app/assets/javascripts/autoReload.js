f4af9f34cb8e02aa10ec89ca8446a77e3e39ad74
jirou6699 <shundom09@gmail.com>
2020-08-18 17:20:54 +0900

    chat-space-automatic-updating

---
+++ app/assets/javascripts/autoReload.js
@@ -0,0 +1,67 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+      `<div class="MessageBox" data-message-id=${message.id}>
+      <div class="MessageInfo">
+      <div class="MessageInfo--name">
+      ${message.user_name}
+      </div>
+      <div class="MessageInfo--date">
+      ${message.created_at}
+      </div>
+      </div>
+      <div class="MessageText">
+      <p class="MessageText__content">
+      ${message.text}
+      </p>
+      <img class="MessageText__image" src="${message.image}">
+      </div>
+      </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="MessageBox" data-message-id=${message.id}>
+      <div class="MessageInfo">
+      <div class="MessageInfo--name">
+      ${message.user_name}
+      </div>
+      <div class="MessageInfo--date">
+      ${message.created_at}
+      </div>
+      </div>
+      <div class="MessageText">
+      <p class="MessageText__content">
+      ${message.text}
+      </p>
+      </div>
+      </div>`
+      return html;
+    };
+  }
+
+  let reloadMessages = function(){
+    let last_message_id = $('.MessageBox:last').data("message-id") || 0;
+    $.ajax({
+      url:"api/messages",
+      type: "get",
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0){
+        let insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.Chat-main__message-list').append(insertHTML);
+        $('.Chat-main__message-list').animate({ scrollTop: $('.Chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      alert('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
+});
+
